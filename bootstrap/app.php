cb0c6e94604e22bcceaa17f24bc1647389fd918f
MishkaSimakov <msimakov661@gmail.com>
2021-05-23 19:49:21 +0300

    install container and routing library, add app service provider

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,4 +6,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . '/..//')->safeLoad();
 
-var_dump(getenv('APP_NAME'));
+require_once __DIR__ . '/container.php';
