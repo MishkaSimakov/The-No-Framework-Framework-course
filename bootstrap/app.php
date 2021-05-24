0ce612259617e09265a1ad0f8ec7e3fccac2170a
MishkaSimakov <msimakov661@gmail.com>
2021-05-24 08:31:23 +0300

    add config service provider

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,15 +6,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createUnsafeImmutable(base_path())->safeLoad();
 
-$arrayLoader = new \App\Config\Loaders\ArrayLoader([
-    'app' => base_path('config/app.php'),
-    'cache' => base_path('config/cache.php'),
-]);
-
-$config = new \App\Config\Config();
-
-$config->load([$arrayLoader]);
-
 require_once __DIR__ . '/container.php';
 
 $route = $container->get(\League\Route\Router::class);
